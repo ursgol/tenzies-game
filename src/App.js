66c854fd3a7bfcd6327ef5bd6a4d1da10446be27
cyber-ula <contact@cyberula.com>
2022-05-05 10:58:53 +0200

    Put real dots on the dice

--- src/App.js
+++ src/App.js
@@ -78,9 +78,9 @@ export default function App() {
 
   return (<main>
     <div className="setup">
-    {tenzies && <Confetti />}
       <div className="title">Tenzies</div>
       <div className="description">
+      {tenzies && <Confetti />}
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       </div>
       <div className="container--1">
--- src/Die.js
+++ src/Die.js
@@ -5,10 +5,60 @@ export default function Die(props){
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
+    const style = {
+        textAlign: "center"
+    }
+
 
     return <div className="item" 
                 style = {styles}
                 onClick={props.holdDice}>
-        <p>{props.value}</p>
+        <p>{props.value === 1? <span className="dot"></span>:
+            props.value ===2?  <div style={style}>
+            <span className="dot"></span>
+            <span className="dot"></span>
+          </div>: 
+          props.value ===3?
+          <div style={style}>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          : props.value ===4 ?
+          <div style={style}>
+            <div className="dot--4">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          <div className="dot--4">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          </div>
+          :props.value === 5 ?
+          <div style={style}>
+              <div className="dot--5">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          <span className="dot dot--1"></span>
+          <div className="dot--5">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          </div>
+          :props.value === 6?
+          <div style={style}>
+              <div className="dot--4">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          <div className="dot--4">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          </div>
+          </div>:props.value}</p>
     </div>
 }
